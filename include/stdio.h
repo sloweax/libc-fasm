da362309973921766fd94dff4e0152c50fc889ab
sloweax <sloweax@users.noreply.github.com>
2023-12-13 20:14:11 -0300

    add `stdout` `stdin` `stderr`

---
+++ include/stdio.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "stddef.h"
+
+struct FILE;
+typedef struct FILE FILE;
+
+extern FILE __stdin;
+extern FILE __stdout;
+extern FILE __stderr;
+
+#define stdin (&__stdint)
+#define stdout (&__stdout)
+#define stderr (&__stderr)
+
+FILE *fopen(const char pathname, const char *mode);
+int fclose(FILE *stream);
+size_t fwrite_unlocked(const void *p, size_t size, size_t n, FILE *stream);
+int fflush_unlocked(FILE *stream);
+int fputc_unlocked(int c, FILE *stream);
--- src/env/_start.asm
+++ src/env/_start.asm
@@ -1,10 +1,26 @@
+include '../stdio/file.inc'
+
 format ELF64
 
 section '.text' executable
 extrn _exit
 extrn main
+extrn __finit
+extrn fflush_unlocked
 public _start
 _start:
+  mov rdi, __stdin
+  mov esi, 0
+  call plt __finit
+
+  mov rdi, __stdout
+  mov esi, 1
+  call plt __finit
+
+  mov rdi, __stderr
+  mov esi, 2
+  call plt __finit
+
   mov edi, dword [rsp]     ; argc
   lea rsi, qword [rsp + 8] ; argv
   lea rdx, [edi*8+16]
@@ -13,9 +29,18 @@ _start:
   mov rax, [rsi]
   mov [__progname], rax
 
+  push rbx
   call plt main
+  mov ebx, eax
 
-  mov edi, eax
+  mov rdi, __stdout
+  call plt fflush_unlocked
+
+  mov rdi, __stderr
+  call plt fflush_unlocked
+  pop rbx
+
+  mov edi, ebx
   call plt _exit
   
 section '.bss' writeable
@@ -24,3 +49,12 @@ environ rq 1
 
 public __progname
 __progname rq 1
+
+public __stdin
+__stdin rb file.sizeof
+
+public __stdout
+__stdout rb file.sizeof
+
+public __stderr
+__stderr rb file.sizeof
